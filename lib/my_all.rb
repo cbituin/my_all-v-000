7991f5bfba8ac75d473f03c0d37eca77d20422d6
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-02 16:51:03 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  blockReturnValues = []
+  while i < collection.length
+    yield(collection[i])
+    blockReturnValues << yield(collection[i])
+    i+=1
+  end
 
+  if blockReturnValues.include?(false)
+    false
+  else
+    true
+  end
+  
 end
